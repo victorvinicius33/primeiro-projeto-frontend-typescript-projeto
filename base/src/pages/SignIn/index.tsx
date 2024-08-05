6c97eb8101bec6492c194861feef3760e3edbc66
Victor Dell <victorvinicius33.vv@gmail.com>
2024-08-05 17:01:20 -0300

    integration sign in page

--- base/src/pages/SignIn/index.tsx
+++ base/src/pages/SignIn/index.tsx
@@ -1,16 +1,51 @@
 import './styles.css';
 import Logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import { FormEvent, useState } from 'react';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      if (!email || !password) {
+        throw new Error('Email and password is required');
+      }
+      const response = await api.post('/login', {
+        email,
+        password
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className='container container-sign-in'>
       <div className='sign-in'>
         <img src={Logo} alt='logo' />
 
-        <form>
-          <input type='text' placeholder='E-mail' />
-          <input type='password' placeholder='Password' />
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text'
+            placeholder='E-mail' 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input 
+            type='password' 
+            placeholder='Password' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
           <span>
             Não tem cadastro?
