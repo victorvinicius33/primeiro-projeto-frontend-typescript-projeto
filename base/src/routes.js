76d88382329b8982595da53c81303353419ca4d2
Victor Dell <victorvinicius33.vv@gmail.com>
2024-08-05 15:28:50 -0300

    creating Sign in page

--- base/src/routes.js
+++ base/src/routes.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import SignIn from './pages/SignIn';
 
 function MainRoutes() {
   return (
     <Routes>
+      <Route path='/' element={<SignIn />} />
       <Route path='/main' element={<Main />} />
     </Routes>
   )
